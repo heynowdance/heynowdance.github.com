343c1af63380b6b662dbd35cfa1b33d23b6d2990
Adam Goldstein <agoldstein@mitre.org>
2011-12-30 11:17:25 -0500

    Converting background images to jpgs to load faster.

--- javascripts/navigation.js
+++ javascripts/navigation.js
@@ -2,11 +2,28 @@ var heynowdance;
 
 heynowdance = this.heynowdance || {};
 
-heynowdance.navigateToView = function(view) {
-  $("#" + view).css({zIndex: 2});
+heynowdance.navigateToView = function(selection) {  
+  // Find the view to which we're switching. Do nothing if selection doesn't exist.
+  var selectedView = heynowdance.views[selection];
+  if (!selectedView) {
+    window.location = window.location.href.replace( /#.*/, "");
+    return;
+  }
+  
+  // Bring the selection to the top behind the current view and then hide current
+  selectedView.css({zIndex: 2});
   heynowdance.currentView.hide();
   
+  // Hide the old view back with the others
   heynowdance.currentView.show();
   heynowdance.currentView.css({zIndex: 0});
-  heynowdance.currentView = $("#" + view);
+  heynowdance.currentView = selectedView;
+  
+  // Set the hash so we can save a link right to this view
+  if (selection != 0)
+    document.location.hash = selection;
+  else {// Almost made a whole site without catering to IE
+    window.location = window.location.href.replace( /#.*/, "");
+    return false;
+  }
 }
