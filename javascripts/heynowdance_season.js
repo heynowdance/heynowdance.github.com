ca0c8da2421f2c38a21c36ae9d9ce73932efce98
Adam Goldstein <agoldstein@mitre.org>
2011-12-30 19:38:51 -0500

    Prev and next season image maps now work for autumn.

--- javascripts/heynowdance_season.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$(".navigation").hover(
-		function() { $(this).find("img").removeClass("hidden"); },
-		function() { $(this).find("img").addClass("hidden"); }
-	);
-});
